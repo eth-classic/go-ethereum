1be160e081cf108c61f6e8027162dd25beff30e5
Austin Abell <austinabell8@gmail.com>
2019-05-06 16:01:38 -0400

    Typo fix

--- core/vm/jump_table_test.go
+++ core/vm/jump_table_test.go
@@ -28,7 +28,7 @@ type ruleSet struct {
 
 func (r ruleSet) IsHomestead(n *big.Int) bool { return n.Cmp(r.hs) >= 0 }
 
-func (r ruleSet) IsAtlantis(n *big.Int) bool { return n.Cmp(r.hs) >= 0 }
+func (r ruleSet) IsAtlantis(n *big.Int) bool { return n.Cmp(r.at) >= 0 }
 
 func (r ruleSet) GasTable(*big.Int) *GasTable {
 	return &GasTable{
